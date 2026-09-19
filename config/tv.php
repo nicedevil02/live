056d30dd158947af30ea0977ffe8efef08de0010
Bahman <nicedevil02@gmail.com>
2026-09-20 02:54:59 +0330

    build(tv): compile new APK v2.0.1 (versionCode 3) and update download links

--- config/tv.php
+++ config/tv.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'latest_version_code' => (int) env('TV_LATEST_VERSION_CODE', 2),
-    'min_version_code'    => (int) env('TV_MIN_VERSION_CODE', 1),
+    'latest_version_code' => (int) env('TV_LATEST_VERSION_CODE', 3),
+    'min_version_code'    => (int) env('TV_MIN_VERSION_CODE', 2),
     'apk_url'             => env('TV_APK_URL', 'https://talalive.ir/downloads/talalive-tv.apk'),
     'base_urls'           => [
         'https://talalive.ir',
