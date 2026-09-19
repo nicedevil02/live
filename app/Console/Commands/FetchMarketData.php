0d0f91c1298f298f605d321ba97fc72a6fb2876c
Bahman <nicedevil02@gmail.com>
2026-09-19 12:48:41 +0330

    docs: update checklist with Phase B commit hash

--- app/Console/Commands/FetchMarketData.php
+++ app/Console/Commands/FetchMarketData.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use App\Services\MarketService;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Carbon;
 
 class FetchMarketData extends Command
 {
@@ -15,10 +15,20 @@ class FetchMarketData extends Command
     {
         $this->info('Fetching market data...');
         
-        Cache::lock('market_fetch_lock', 25)->get(function () use ($service) {
-            $service->refreshIfStale($service->currentRefreshIntervalSeconds());
-            Cache::put('market_last_fetch_at', now()->toISOString(), 3600);
-        });
+        $now = Carbon::now('Asia/Tehran');
+        $hour = $now->hour;
+        
+        // در ساعات کاری (۹ الی ۲۲): بروزرسانی هر ۱۰ ثانیه (۶ بار در دقیقه)
+        $iterations = ($hour >= 9 && $hour < 22) ? 6 : 1;
+        $interval = ($hour >= 9 && $hour < 22) ? 10 : 60;
+
+        for ($i = 0; $i < $iterations; $i++) {
+            $service->refreshIfStale($interval);
+
+            if ($i < $iterations - 1) {
+                sleep(10);
+            }
+        }
         
         $this->info('Done.');
         return 0;
