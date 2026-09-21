c7ce0611f5c4f408f9e5e4aa1edcb9bef32150da
Bahman <nicedevil02@gmail.com>
2026-09-21 13:50:37 +0330

    chore: remove one-time theme migration logic and temporary migration file

--- app/Http/Controllers/PublicDisplayController.php
+++ app/Http/Controllers/PublicDisplayController.php
@@ -209,7 +209,6 @@ class PublicDisplayController extends Controller
         $resolved = $this->resolveGalleryCityInfo($user, $settings);
 
         \App\Http\Controllers\Admin\DisplaySettingController::ensureEmptyShowcaseColumnsExist();
-        \App\Http\Controllers\Admin\DisplaySettingController::ensureAllThemesUpdatedToLightModernOnce();
 
         $settingsData = $settings->toArray();
         $settingsData['city_slug'] = $resolved['citySlug'];
